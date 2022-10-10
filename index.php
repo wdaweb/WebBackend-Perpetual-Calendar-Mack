d8a3795ca0b731685fed7d19d2be2e98b5761e69
mackliu <macklun@gmail.comm>
2022-10-10 16:13:35 +0800

    11102-萬年曆作業說明內容編修

--- index.php
+++ index.php
@@ -1,11 +1,19 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="shortcut icon" href="#">
   <title>萬年曆作業</title>
   <style>
    /*請在這裹撰寫你的CSS*/ 
     
   </style>
+</head>
 <body>
-<h1>萬年曆</h1>
+<h1>萬年曆</h1>  
+
 <?php
 /*請在這裹撰寫你的萬年曆程式碼*/  
   
@@ -13,4 +21,4 @@
 ?>
   
 </body>
-<html>
+</html>
